4ceb88c91095c365712cc6f5e4c210f86897b447
fstwon <fstwon@gmail.com>
2025-05-21 19:26:55 +0900

    merge: merge branch from develop

--- src/components/pagination/Pagination.component.jsx
+++ src/components/pagination/Pagination.component.jsx
@@ -4,6 +4,7 @@ import './pagination.style.scss';
 /**
  * 총 아이템 수
  * 페이지 단위
+ * 페이지 당 아이템 요청 수
  *
  *
  * @param {*} param0
@@ -11,7 +12,7 @@ import './pagination.style.scss';
  */
 
 export const Pagination = ({ className }) => {
-	return (
-		<span className={classNames('pagination__container', className)}></span>
-	);
+  return (
+    <span className={classNames('pagination__container', className)}></span>
+  );
 };
