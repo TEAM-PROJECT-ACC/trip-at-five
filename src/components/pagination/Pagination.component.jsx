13b61c50d13e010005deb8a68b0d60566935d56b
fstwon <fstwon@gmail.com>
2025-05-21 21:20:57 +0900

    feat: 공용 pagination 컴포넌트 ui 구현

--- src/components/pagination/Pagination.component.jsx
+++ src/components/pagination/Pagination.component.jsx
@@ -1,18 +1,58 @@
 import { classNames } from '../../utils/index';
+import { PageButton } from './components/page-button/PageButton.component';
+import {
+  FaAngleDoubleLeft,
+  FaAngleDoubleRight,
+  FaAngleLeft,
+  FaAngleRight,
+} from '../../assets/icons/index';
 import './pagination.style.scss';
+import { PageList } from './components/page-list/PageList.component';
 
 /**
- * 총 아이템 수
- * 페이지 단위
- * 페이지 당 아이템 요청 수
  *
  *
- * @param {*} param0
+ * @param {
+ * totalCount 총 아이템 수
+ * pageLength 페이지 단위
+ * currentPage 현재 페이지
+ * numOfRows 페이지 당 아이템 요청 수
+ * }
  * @returns
  */
 
-export const Pagination = ({ className }) => {
+export const Pagination = ({
+  className,
+  totalCount,
+  pageLength,
+  currentPage,
+  numOfRows,
+  useMoveToEnd,
+}) => {
   return (
-    <span className={classNames('pagination__container', className)}></span>
+    <div className={classNames('global-pagination__container', className)}>
+      {useMoveToEnd && (
+        <PageButton>
+          <FaAngleDoubleLeft />
+        </PageButton>
+      )}
+      <PageButton>
+        <FaAngleLeft />
+      </PageButton>
+      <PageList
+        totalCount={totalCount}
+        pageLength={pageLength}
+        currentPage={currentPage}
+        numOfRows={numOfRows}
+      />
+      <PageButton disabled={true}>
+        <FaAngleRight />
+      </PageButton>
+      {useMoveToEnd && (
+        <PageButton>
+          <FaAngleDoubleRight />
+        </PageButton>
+      )}
+    </div>
   );
 };
