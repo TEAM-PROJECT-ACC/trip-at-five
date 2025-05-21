40d1d95055b41c06f99df52bb25d00543f87995d
fstwon <fstwon@gmail.com>
2025-05-21 09:52:27 +0900

    fix: test 페이지 구분용 스타일 추가

--- src/pages/test/Test.page.jsx
+++ src/pages/test/Test.page.jsx
@@ -4,14 +4,23 @@ import {
 	InputShrink,
 } from '../../components/inputs';
 import { ClassNamesTest } from '../../utils';
+import './test.style.scss';
 
 export const TestPage = () => {
 	return (
 		<div className='test-page__container'>
 			<ClassNamesTest />
-			<InputPrimary />
-			<InputSecondary />
-			<InputShrink labelText={'라벨'} />
+			<section className='test-page__section'>
+				<h1>전역 (공용) 컴포넌트 확인</h1>
+				<h3>input component</h3>
+				<div className='test-page__test-area'>
+					<InputPrimary />
+					<InputSecondary />
+					<InputShrink labelText={'라벨'} />
+				</div>
+			</section>
 		</div>
 	);
 };
+
+//test-page__section
