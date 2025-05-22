f2427387d5f3bf6624271c6aaeceb5985e3dbff3
Seong-Jun1525 <sjsj123455@naver.com>
2025-05-22 14:11:12 +0900

    Modify: 예약페이지 테스트 링크 제거 및 strictmode 활성화

--- src/pages/main/Main.page.jsx
+++ src/pages/main/Main.page.jsx
@@ -5,8 +5,6 @@ import MainArea from './local-components/MainArea.component';
 const Main = () => {
   return (
     <div className='main-page__container'>
-      {/* 추후 삭제할 예정 */}
-      <a href='accommodations/1/reservations'>예약페이지</a>
       <MainArea />
     </div>
   );
