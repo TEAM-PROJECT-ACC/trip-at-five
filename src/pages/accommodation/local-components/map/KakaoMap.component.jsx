93f245395059e1fa1073dbb84ce9d3df7cc65812
yoonseo <145835135+yoonseo0832@users.noreply.github.com>
2025-05-22 10:32:24 +0900

    merge: develop 병합 전 중간 저장

--- src/pages/accommodation/local-components/map/KakaoMap.component.jsx
+++ src/pages/accommodation/local-components/map/KakaoMap.component.jsx
@@ -2,11 +2,11 @@ import { useEffect, useRef } from "react";
 import "./KakaoMap.style.scss";
 import FilterPanel from "../filter/FilterPanel.component";
 import { MapInnerList } from "../acc/MapInnerList.component";
-import { markedData } from "./mapMarker";
 import { TiDelete } from "../../../../assets/icons/ys/index";
 
 export const KakaoMap = ({ onClose }) => {
   const mapRef = useRef();
+
   useEffect(() => {
     if (!window.kakao || !window.kakao.maps) return;
 
@@ -16,6 +16,7 @@ export const KakaoMap = ({ onClose }) => {
         console.error("Map container not found");
         return;
       }
+
       const mapOptions = {
         center: new window.kakao.maps.LatLng(37.566826, 126.9786567),
         level: 3,
@@ -24,14 +25,24 @@ export const KakaoMap = ({ onClose }) => {
       const map = new window.kakao.maps.Map(mapContainer, mapOptions);
 
       markedData.forEach((el) => {
-        new kakao.maps.Marker({
-          map: map,
+        new window.kakao.maps.Marker({
           position: new kakao.maps.LatLng(el.lat, el.lng),
-          title: el.title,
+          map: map,
+        });
+
+        const overlayContent = document.createElement("div");
+        overlayContent.innerText = el.content;
+        overlayContent.style.cssText =
+          "background:white; padding:5px; border:1px solid #333;";
+
+        const customOverlay = new window.kakao.maps.CustomOverlay({
+          content: overlayContent,
+          position: marker.getPosition(),
+          yAnchor: 1,
         });
       });
-      const marker = new window.kakao.maps.Marker({ position: markerPosition });
-      marker.setMap(map);
+
+      customOverlay.setMap(map);
     });
   }, []);
 
