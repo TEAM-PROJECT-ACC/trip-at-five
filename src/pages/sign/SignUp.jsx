5e6b36e4075e33d6a036745ff8665bd85e206dde
kkh1396 <yuioop1396@gmail.com>
2025-05-23 11:50:05 +0900

    merge: pr로 인한 커밋진행

--- src/pages/sign/SignUp.jsx
+++ src/pages/sign/SignUp.jsx
@@ -9,13 +9,13 @@ export default function SignUp() {
     return(
         <>
          <div className='sign-wrap'>
-        <TitleText className={'sign-title'}/>
+        {/* <TitleText className={'sign-title'}/>
         <Circle/>
         <div className='sign-input'>
         <p className='sign-input-item-text'>이메일 *</p>
         <InputPrimary className='sign-input-item'/>
         <ButtonPrimary className='sign-input-item'>이메일로 인증</ButtonPrimary>
-        </div>
+        </div> */}
         </div>
         </>
     )
