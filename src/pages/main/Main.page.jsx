c4071acecfeabf09277f5fd6d6d210738709efb0
Seong-Jun1525 <sjsj123455@naver.com>
2025-05-24 13:28:09 +0900

    Fix: 검색창 인원수 색 안칠해지는 버그 수정

--- src/pages/main/Main.page.jsx
+++ src/pages/main/Main.page.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Main.style.scss';
 import MainArea from './local-components/MainArea.component';
+import { useAccomSearchStore } from '../../states';
 
 const Main = () => {
+  const state = useAccomSearchStore((state) => state);
+
+  useEffect(() => {
+    // 새로고침 할 경우 검색 상태 초기화
+    state.resetState();
+  }, []);
+
   return (
     <div className='main-page__container'>
       <MainArea />
