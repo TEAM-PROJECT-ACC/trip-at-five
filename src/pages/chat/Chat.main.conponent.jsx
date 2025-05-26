8ad9a94e6641d080dbeaed7b251049d7d021e58c
kkh1396 <yuioop1396@gmail.com>
2025-05-26 19:30:53 +0900

    Feat: chat 문의하기 페이지 작업

--- src/pages/chat/Chat.main.conponent.jsx
+++ src/pages/chat/Chat.main.conponent.jsx
@@ -1,12 +1,24 @@
-import { PageContainer } from '../../components';
-import './chat.main.component.scss';
+import { InputPrimary, PageContainer, Select } from '../../components';
+import { ChatTitle } from './chat-title/Chat.title.conponent';
+import './chat.main.conponent.scss';
 
 export function Chat() {
 	return (
 		<PageContainer>
-			<div className='login-wrap'>
+			<div className='chat-wrap'>
+				<ChatTitle
+					className={'chat-top-title'}
+					text={'문의하기'}
+				/>
 
-      </div>
+				<InputPrimary className={'chat-content-input'} />
+				<Select className={'chat-content-select'}
+					optionList={[
+						{ value: '1', label: '예약문의' },
+						{ value: '2', label: '기타문의' },
+					]}
+				/>
+			</div>
 		</PageContainer>
 	);
 }
