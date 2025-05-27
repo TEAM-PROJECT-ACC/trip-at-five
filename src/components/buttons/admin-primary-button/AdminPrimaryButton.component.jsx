8f041eb1814a22fe800c0147b35356001ac813f4
Seong-Jun1525 <sjsj123455@naver.com>
2025-05-27 11:46:27 +0900

    Feat: 객실 등록 유효성 검사 구현

--- src/components/buttons/admin-primary-button/AdminPrimaryButton.component.jsx
+++ src/components/buttons/admin-primary-button/AdminPrimaryButton.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './AdminPrimaryButton.style.scss';
 import { classNames } from '../../../utils';
 
-const AdminPrimaryButton = ({ className, onClick, children }) => {
+const AdminPrimaryButton = ({ className, onClick, children, ...props }) => {
   return (
-    <button className={classNames('admin-button', className)} onClick={onClick}>
+    <button className={classNames('admin-button', className)} onClick={onClick} {...props}>
       {children}
     </button>
   );
