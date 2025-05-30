a67e79508e6aa465a77fc304aa7d26531cfa21e1
Seong-Jun1525 <sjsj123455@naver.com>
2025-05-30 23:56:51 +0900

    Modify: 임시 저장

--- src/services/room/roomService.api.js
+++ src/services/room/roomService.api.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
-import { apiAxios } from '../service';
+import { serverBaseURL } from '../serverBaseURL';
+
+const apiAxios = axios.create({
+  baseURL: serverBaseURL,
+  withCredentials: true,
+});
 
 export const roomInsertAPI = async (roomData) => {
   const response = await apiAxios.post('/rooms', roomData);
+  console.log(response.data);
 };
