544bdb506541e07faae937d06723215abb5e6439
fstwon <fstwon@gmail.com>
2025-06-17 11:34:49 +0900

    Feature: diary api 회원 정보 로그인 정보로 수정

--- src/pages/diary/components/diary-list/DiaryList.component.jsx
+++ src/pages/diary/components/diary-list/DiaryList.component.jsx
@@ -4,42 +4,62 @@ import { useDiaryList } from '../../hooks/useDiaryList.hook';
 import { DiaryProvider } from '../../stores';
 import { DiaryItem } from '../diary-item/DiaryItem.component';
 import './diaryList.style.scss';
+import { classNames } from '../../../../utils';
+import { loginStateStore } from '../../../../states/login/loginStore';
 
 export const DiaryList = () => {
+  const { loginInfo } = loginStateStore();
   const { pageInfo, diaryList, selectAllList } = useDiaryList();
   const currentPage = useMemo(() => {
     return pageInfo.pageNo;
   }, [pageInfo]);
-
+  console.log(pageInfo);
   // TODO: memNo를 로그인 회원의 memNo로 수정해야 함
   const handleCurrentPage = (pageNo) => {
     if (pageNo !== currentPage) {
-      selectAllList({ memNo: 2, pageNo, numOfRows: pageInfo.numOfRows });
+      selectAllList({
+        memNo: loginInfo.memSq,
+        pageNo,
+        numOfRows: pageInfo.numOfRows,
+      });
     }
   };
 
   return (
     <>
-      <div className='diary-page__diary-list-container'>
-        {diaryList &&
-          diaryList.length > 0 &&
-          diaryList.map((diary) => {
-            return (
-              <DiaryProvider
-                key={diary.diarySq}
-                initialDiary={diary}
-              >
-                <DiaryItem initDiary={diary} />
-              </DiaryProvider>
-            );
-          })}
+      <div
+        className={classNames(
+          'diary-page__diary-list-container',
+          !diaryList || diaryList.length === 0 ? 'no-list' : ''
+        )}
+      >
+        {diaryList && diaryList.length > 0 ? (
+          <>
+            {diaryList.map((diary) => {
+              return (
+                <DiaryProvider
+                  key={diary.diarySq}
+                  initialDiary={diary}
+                >
+                  <DiaryItem initDiary={diary} />
+                </DiaryProvider>
+              );
+            })}
+          </>
+        ) : (
+          <div className='diary-page__no-list-container'>
+            아직 작성된 일지가 없습니다.
+          </div>
+        )}
       </div>
-      <Pagination
-        className='diary-page__pagination'
-        totalCount={pageInfo.totalCount}
-        currentPage={currentPage}
-        onClick={handleCurrentPage}
-      />
+      {diaryList && diaryList.length > 0 && (
+        <Pagination
+          className='diary-page__pagination'
+          totalCount={pageInfo.totalCount}
+          currentPage={currentPage}
+          onClick={handleCurrentPage}
+        />
+      )}
     </>
   );
 };
